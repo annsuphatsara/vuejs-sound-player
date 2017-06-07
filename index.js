d47004ba05951c18bfeaf85f49b22831c7610fc0
suphatsara <suphatsara.laoh@gmail.com>
2017-06-07 14:20:12 +0700

    edit export

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // import VueAudio from './src/VueAudio.vue';
-// var VueAudio = required('./src/VueAudio.vue');
+var VueAudio = required('./src/VueAudio.vue');
 
 // export default VueAudio;
-export default {
-  data() {
-    return {
-      VueAudio: required('./src/VueAudio.vue')
-    };
-  }
-};
+// export default {
+//   data() {
+//     return {
+//       VueAudio
+//     };
+//   }
+// };
 export { VueAudio };
